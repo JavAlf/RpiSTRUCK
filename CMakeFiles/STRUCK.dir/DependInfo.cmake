29280e46ea77bc41031b7ce734aab0ef79d61342
francisco <frj.alfonso@gmail.com>
2015-03-25 14:53:11 +0100

    Initial commit

---
+++ CMakeFiles/STRUCK.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/francisco/Documents/tryStr/t2STRUCK/Config.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/Config.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/Features.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/Features.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/GraphUtils.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/GraphUtils.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/HaarFeature.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/HaarFeature.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/HaarFeatures.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/HaarFeatures.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/HistogramFeatures.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/HistogramFeatures.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/ImageRep.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/ImageRep.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/LaRank.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/LaRank.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/MultiFeatures.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/MultiFeatures.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/RawFeatures.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/RawFeatures.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/Sampler.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/Sampler.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/Tracker.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/Tracker.cpp.o"
+  "/home/francisco/Documents/tryStr/t2STRUCK/main.cpp" "/home/francisco/Documents/tryStr/t2STRUCK/CMakeFiles/STRUCK.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/GraphUtils"
+  "/usr/include/eigen2"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/STRUCK.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STRUCK.dir/Config.cpp.o"
+  "CMakeFiles/STRUCK.dir/Features.cpp.o"
+  "CMakeFiles/STRUCK.dir/HaarFeature.cpp.o"
+  "CMakeFiles/STRUCK.dir/HaarFeatures.cpp.o"
+  "CMakeFiles/STRUCK.dir/HistogramFeatures.cpp.o"
+  "CMakeFiles/STRUCK.dir/ImageRep.cpp.o"
+  "CMakeFiles/STRUCK.dir/LaRank.cpp.o"
+  "CMakeFiles/STRUCK.dir/main.cpp.o"
+  "CMakeFiles/STRUCK.dir/MultiFeatures.cpp.o"
+  "CMakeFiles/STRUCK.dir/RawFeatures.cpp.o"
+  "CMakeFiles/STRUCK.dir/Sampler.cpp.o"
+  "CMakeFiles/STRUCK.dir/Tracker.cpp.o"
+  "CMakeFiles/STRUCK.dir/GraphUtils.cpp.o"
+  "STRUCK.pdb"
+  "STRUCK"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STRUCK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
